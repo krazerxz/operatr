34929f4383aebe3c15f6b1c32a108d6613f64841
Chris Pomfret <cpom40ba@gmail.com>
2017-05-08 17:30:35 +0100

    still no luck

--- app/assets/javascripts/operatr.js
+++ app/assets/javascripts/operatr.js
@@ -1,19 +1,42 @@
-$(document).ready(function(){
-  var audioContext = new AudioContext();
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-  var peer = new Peer();
-  var peer2 = new Peer();
+$(document).ready(function(){
+
+  var peer = new Peer({key: 'b5q5smtw3dr96bt9', debug: 3});
   var constraints = { audio: true, video: false };
 
-  navigator.mediaDevices.getUserMedia(constraints).then(function (mediaStream) {
+  peer.on('open', function(id){
+    $('#pid').text(id);
+    $('#status').text("connected");
 
-    peer.on('call', function(call) {
-      call.answer(mediaStream);
-    });
-
-    peer.on('open', function(id){
-      var outgoing_call = peer2.call(id, mediaStream);
-    });
-
+    navigator.getUserMedia(constraints, function(stream){
+      window.localStream = stream;
+    }, function(){ console.log("og"); });
   });
+
+  peer.on('call', function(call) {
+    call.answer(window.localStream);
+    closeExistingCall();
+    setNewCall(call);
+  });
+
+  function setNewCall(call) {
+    window.existingCall = call;
+  }
+
+  function closeExistingCall() {
+    if (window.existingCall) {
+      window.existingCall.close();
+    }
+  }
+
+  // Make call
+  $('#call').click(function() {
+    var requestedPeer = $('#rid').val();
+    var call = peer.call(requestedPeer, window.localStream);
+    closeExistingCall();
+    setNewCall(call);
+  });
+
+
 });
