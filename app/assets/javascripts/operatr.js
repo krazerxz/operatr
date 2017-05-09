2ed8b98d7c4a81bba0c59189d5f22ee9b729488c
Chris Pomfret <cpom40ba@gmail.com>
2017-05-09 08:42:11 +0100

    doing some pruning

--- app/assets/javascripts/operatr.js
+++ app/assets/javascripts/operatr.js
@@ -1,17 +1,15 @@
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
 $(document).ready(function(){
 
   var peer = new Peer({key: 'b5q5smtw3dr96bt9', debug: 3});
   var constraints = { audio: true, video: false };
 
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream){
+    window.localStream = stream;
+  });
+
   peer.on('open', function(id){
     $('#pid').text(id);
     $('#status').text("connected");
-
-    navigator.getUserMedia(constraints, function(stream){
-      window.localStream = stream;
-    }, function(){ window.alert("Get a better browser"); });
   });
 
   peer.on('call', function(call) {
@@ -36,13 +34,11 @@ $(document).ready(function(){
     }
   }
 
-  // Make call
   $('#call').click(function() {
     var requestedPeer = $('#rid').val();
     var call = peer.call(requestedPeer, window.localStream);
     closeExistingCall();
     setNewCall(call);
   });
-
 
 });
