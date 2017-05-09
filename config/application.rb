5f5950721eab6d0aac34f94d59628b2c7408bcf8
Chris Pomfret <cpom40ba@gmail.com>
2017-05-09 14:54:49 +0100

    just adding pg will be easier :|

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,9 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
-#require "active_model/railtie" # Until we have databases
+require "active_model/railtie"
 require "active_job/railtie"
-#require "active_record/railtie" #Until we have databases
+require "active_record/railtie"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
 require "action_view/railtie"
